73a9ec020e5919000fa9f7801f49225e2989270d
Théo Mesnil <mesniltheo@gmail.com>
2021-12-28 19:41:08 +0100

    chore: first commit
    Signed-off-by: Théo Mesnil <mesniltheo@gmail.com>

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, Button, Alert} from 'react-native';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  const sendMessageToAppleWatch = () => {
+    Alert.alert(`the message "${message}" has been sent`);
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        value={message}
+        onChangeText={setMessage}
+        placeholder="message"
+      />
+      <Button onPress={sendMessageToAppleWatch} title="SEND" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
